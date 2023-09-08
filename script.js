b1fdd5e7cc84ee426935004b1e0bd5036d507882
Faleye Idris <idris@Faleyes-MacBook-Pro.local>
2023-09-08 22:55:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const day = document.querySelector(".day");
+const date = document.querySelector(".date");
+const defaultDate = new Date();
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+day.textContent = days[defaultDate.getDay()];
+date.textContent = `${Math.ceil(defaultDate.getUTCMilliseconds())}s`;
